cff5b7e0c25af3c61bb7e344e5e953a49dfe21d6
rohskopf <drew.rohskopf@gmail.com>
2017-06-19 11:38:30 -0400

    Old src files

---
+++ pops-old/src/ga2.h
@@ -0,0 +1,6 @@
+// This is start of the header guard.  ADD_H can be any unique name.  By convention, we use the name of the header file.
+#ifndef GA2_H
+#define GA2_H
+
+int gen_num;
+int g; // generation variable
